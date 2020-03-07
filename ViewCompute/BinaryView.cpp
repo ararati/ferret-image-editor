f2b3512c37e70d649b983ca3d28fcbcc17c10d32
Ararat Poghosyan <mistermoondi@gmail.com>
2020-03-07 23:33:48 +0200

    Add RGB binary

--- ViewCompute/BinaryView.cpp
+++ ViewCompute/BinaryView.cpp
@@ -1,5 +1,6 @@
 #include "BinaryView.h"
 #include "GrayscaleView.h"
+#include <QtDebug>
 
 void BinaryView::process(Image *original, Image *processed, int treshold)
 {
@@ -11,6 +12,23 @@ void BinaryView::process(Image *original, Image *processed, int treshold)
             QRgb px = original->pixel(x, y);
             int bin = (qBlue(px) >= treshold) ? 255 : 0;
             processed->setPixel(x, y, qRgb(bin, bin, bin));
+        }
+    }
+}
+
+void BinaryView::binaryColor(Image *original, Image *processed, int rT, int gT, int  bT)
+{
+    uint height = processed->height(), width = processed->width(), r, g, b;
+    for(int x = 0; x < width; x++)
+    {
+        for(int y = 0; y < height; y++)
+        {
+            QRgb px = original->pixel(x, y);
+            r = (qRed(px) >= rT) ? 255 : 0;
+            g = (qGreen(px) >= gT) ? 255 : 0;
+            b = (qBlue(px) >= bT) ? 255 : 0;
+
+            processed->setPixel(x, y, qRgb(r, g, b));
         }
     }
 }
--- ViewCompute/BinaryView.h
+++ ViewCompute/BinaryView.h
@@ -9,6 +9,7 @@ class BinaryView
 {
 public:
     static void process(Image* original, Image* image, int treshold);
+    static void binaryColor(Image *original, Image *processed, int rT, int gT, int bT);
 };
 
 #endif // BINARYVIEW_H
