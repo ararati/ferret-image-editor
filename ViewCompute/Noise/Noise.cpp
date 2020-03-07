5848548ec8e57b10eb2d76f74045d451ec4cb033
Ararat Poghosyan <mistermoondi@gmail.com>
2020-03-07 23:34:05 +0200

    Add biopolar noise

--- ViewCompute/Noise/Noise.cpp
+++ ViewCompute/Noise/Noise.cpp
@@ -8,7 +8,7 @@ default_random_engine rngY;
 
 void Noise::proccess(Image *original, Image *processing, unsigned int countWhite, unsigned int countBlack)
 {
-    int height = processing->height(), width = processing->width();
+    int height = processing->height()-1, width = processing->width()-1;
 
     int rX, rY;
     for(int i = 0; i < countWhite; i++)
@@ -31,5 +31,24 @@ void Noise::proccess(Image *original, Image *processing, unsigned int countWhite
             rY = randY(rngX);
 
             processing->setPixel(rX, rY, qRgb(0, 0, 0));
+    }
+}
+
+void Noise::bipolar(Image *original, Image *processing, unsigned int count)
+{
+    int height = processing->height()-1, width = processing->width()-1;
+
+    int rX, rY;
+    for(int i = 0; i < count; i++)
+    {
+            uniform_int_distribution<int> randX(1, width-1);
+            uniform_int_distribution<int> randY(1, height-1);
+
+            rX = randX(rngX);
+            rY = randY(rngX);
+
+            processing->setPixel(rX, rY, qRgb(255, 255, 255));
+            processing->setPixel(rX+1, rY+1, qRgb(0, 0, 0));
+            processing->setPixel(rX-1, rY-1, qRgb(0, 0, 0));
     }
 }
--- ViewCompute/Noise/Noise.h
+++ ViewCompute/Noise/Noise.h
@@ -9,6 +9,7 @@ class Noise
 {
 public:
     static void proccess(Image *original, Image *processed, unsigned int countWhite, unsigned int countBlack);
+    static void bipolar(Image *original, Image *processing, unsigned int count);
 };
 
 #endif // NOISE_H
