43b5e5aedbd2863e5baa10b85d3912e329b9b3c4
Ararat Poghosyan <mistermoondi@gmail.com>
2020-03-24 23:49:40 +0200

    Fix error of transform position

--- ViewCompute/TransformPosition.cpp
+++ ViewCompute/TransformPosition.cpp
@@ -8,16 +8,16 @@ void TransformPosition::transformX(Image *original, Image *processImg, int step)
     int startCol = (step > 0) ? width : 0;
     step *= -1;
 
-    for(int row = 0; row < height; row++)
+    for(int x = 0; x < width; x++)
     {
-        for(int col = 0; col < width; col++)
+        for(int y = 0; y < height; y++)
         {
             //With repeat image
             //processImg->getCvImg().at<Vec3b>(row, col) = original->getCvImg().at<Vec3b>(row, startCol+col+step);
-//            if(col + step < width)
-//                processImg->getCvImg().at<Vec3b>(row, col) = original->getCvImg().at<Vec3b>(row, startCol+col+step);
-//            else
-//                processImg->getCvImg().at<Vec3b>(row, col) = Vec3b(255, 255, 255);
+            if(x + step < width)
+                processImg->setPixel(x, y, original->pixel(x+step, y));
+            else
+                processImg->setPixel(x, y, qRgb(255, 255, 255));
 
         }
     }
